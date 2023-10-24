e0923446fa5543577b4804dd96a13be0477b040c
Natalia Rosa <nataliashelnutt@gmail.com>
2023-10-24 08:39:44 -0300

    Prática de conversão de valores

--- src/DesafioTemperatura.java
+++ src/DesafioTemperatura.java
@@ -1,5 +1,15 @@
-public class Main {
+public class DesafioTemperatura {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double temperaturaEmCelsius = 30.4;
+        double temperaturaEmFahrenheit = (temperaturaEmCelsius * 1.8) + 32;
+
+        String mensagem = String.format("A temperatura de %f Celsius é equivalente a %f Fahrenheit", temperaturaEmCelsius, temperaturaEmFahrenheit);
+
+        System.out.println(mensagem);
+
+        int temperaturaEmFahrenheitInteira = (int) temperaturaEmFahrenheit;
+        System.out.println("A temperatura em Fahrenheit inteira é: " + temperaturaEmFahrenheitInteira);
+
     }
 }
